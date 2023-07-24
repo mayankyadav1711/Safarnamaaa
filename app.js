136698b86c7f6f04708280069b7e1f3ae470f255
mayankyadav1711 <114771432+mayankyadav1711@users.noreply.github.com>
2023-07-24 23:52:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(require('./routes/user'))
 
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static('client/build'))
+    app.use(express.static('build'))
     const path = require('path')
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'build','index.html'))
     })
 }
 
