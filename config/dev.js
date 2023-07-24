72a55ca7251f1a1f407284aa3338d96d8a765639
mayankyadav1711 <114771432+mayankyadav1711@users.noreply.github.com>
2023-07-24 23:41:44 +0530

    change

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://hellosafarnamaaa:ihL9I13v0dtDuLaY@safarnamaaa.tgf9qbd.mongodb.net/",
+    JWT_SECRET:"shfgiahoiauhroiuha",
+    EMAIL:"http://localhost:3000",
+    GPASS:"dnoevwrblknojobh"
+}
