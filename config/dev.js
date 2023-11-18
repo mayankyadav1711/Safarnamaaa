58f655cb7526ea2a73d2e5f742a7f0e6ed8bb24b
Mayank Yadav <114771432+mayankyadav1711@users.noreply.github.com>
2023-11-18 16:04:47 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,1 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://hellosafarnamaaa:ihL9I13v0dtDuLaY@safarnamaaa.tgf9qbd.mongodb.net/",
-    JWT_SECRET:"shfgiahoiauhroiuha",
-    EMAIL:"http://localhost:3000",
-    GPASS:"dnoevwrblknojobh"
-}
+
